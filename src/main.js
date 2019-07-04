aa9b09e098be99c35e545065bdb8e47fde980b8d
yujiansheng <2264915250@qq.com>
2019-07-04 10:30:23 +0800

    第一次修改

--- src/main.js
+++ src/main.js
@@ -1,1 +1,17 @@
 //入口
+import Vue from 'vue'
+
+//按需导入 Mint-ui
+import {Header} from 'mint-ui'
+
+import './lib/mui/css/mui.css'
+
+//导入根组件
+import app from './APP.vue'
+
+Vue.component(Header.name,Header)
+
+var vm = new Vue({
+    el:"#app",
+    render:h=>h(app)
+})
