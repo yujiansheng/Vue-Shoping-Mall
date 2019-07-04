0372d02987c5b4343126a07d2334850bd9dd371f
yujiansheng <2264915250@qq.com>
2019-07-04 09:24:10 +0800

    test

--- src/APP.vue
+++ src/APP.vue
@@ -1,46 +1,13 @@
 <template>
-  <div>
-    <h1>这是APP组件</h1>
-    <mt-button type="danger" size="small" plain icon="more" @click="show">default</mt-button>
-    <router-link to="/account">Account</router-link>
-    <router-link to="/goodslist">Goodslist</router-link>
-    <router-view></router-view>
-    <button type="button" class="mui-btn mui-btn-royal">
-					紫色
-				</button>
-  </div>
 </template>
 <script>
-import { Toast } from "mint-ui";
-
 export default {
-  name: "App",
-  data() {
+  name: "component_name",
+  data () {
     return {
-      toastInstanse:null
     };
-  },
-  created() {
-    this.getList();
-  },
-  methods: {
-    getList(){
-      this.show();
-      setTimeout(() => {
-        this.toastInstanse.close();
-      }, 3000);
-    },
-    show() {
-      this.toastInstanse=Toast({
-        message: "提示消息",
-        position: "top",
-        duration: -1,
-        iconClass:'glyphicon glyphicon-heart',
-        className:'mytoast'
-      });
-    }
   }
-};
+}
 </script>
 <style lang="scss" scoped>
 </style>
--- src/router.js
+++ src/router.js
@@ -1,30 +1,10 @@
-import account from './main/Account.vue'
-import goodslist from './main/GoodslList.vue'
-import login from './subcom/login.vue'
-import register from './subcom/register.vue'
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 //手动安装 vuerouter
 Vue.use(VueRouter);
 
 var router = new VueRouter({
-    routes: [{
-            path: '/account',
-            component: account,
-            children: [{
-                    path: 'login',
-                    component: login
-                },
-                {
-                    path: 'register',
-                    component: register
-                },
-            ]
-        },
-        {
-            path: '/goodslist',
-            component: goodslist
-        }
+    routes: [
     ]
 })
 
