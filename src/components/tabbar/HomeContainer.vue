0a7f9aa7af978852f3d00914be556f2c1ed0389c
yujiansheng <2264915250@qq.com>
2019-07-12 22:33:29 +0800

    科比

--- src/components/tabbar/HomeContainer.vue
+++ src/components/tabbar/HomeContainer.vue
@@ -47,8 +47,8 @@ export default {
     return {
         lunbotulist:[
             {url:12,img:'https://desk-fd.zol-img.com.cn/t_s960x600c5/g5/M00/06/08/ChMkJ1ZXvYuIBgwTAAS3zVB-lRIAAFZcgEg_uUABLfl181.jpg'},
-            {url:13,img:'https://gss1.bdstatic.com/-vo3dSag_xI4khGkpoWK1HF6hhy/baike/c0%3Dbaike933%2C5%2C5%2C933%2C330/sign=c6d1917fd958ccbf0fb1bd6878b1d75b/8435e5dde71190ef03c9aa12c61b9d16fcfa60c8.jpg'},
-            {url:14,img:'https://desk-fd.zol-img.com.cn/t_s960x600c5/g5/M00/06/08/ChMkJlZXvYqIPy0OAAJWwmQJENgAAFZcgC8OioAAlba296.jpg'},
+            {url:13,img:'https://desk-fd.zol-img.com.cn/t_s960x600c5/g5/M00/06/08/ChMkJlZXvYqIPy0OAAJWwmQJENgAAFZcgC8OioAAlba296.jpg'},
+            {url:14,img:'https://gss0.bdstatic.com/94o3dSag_xI4khGkpoWK1HF6hhy/baike/c0%3Dbaike92%2C5%2C5%2C92%2C30/sign=ea8aabe1a40f4bfb98dd960662261395/5ab5c9ea15ce36d3bdb1474e3af33a87e950b113.jpg'},
             {url:15,img:'https://desk-fd.zol-img.com.cn/t_s960x600c5/g5/M00/06/08/ChMkJ1ZXvY2IB6zUAAWs1p-R0l8AAFZcgIrkVsABazu054.jpg'}
 
         ]//保存轮播图的数据
